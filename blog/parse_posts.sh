bdb029489c0cd571b15a8aea6e394fcd76d8055a
Dylan M Taylor <dylan.m.taylor92@gmail.com>
2015-04-20 09:26:37 +0000

    improve parsing

--- blog/parse_posts.sh
+++ blog/parse_posts.sh
@@ -1,5 +1,8 @@
+echo "update image urls"
 find . -type f -exec sed -i 's/http:\/\/dylanmtaylor.com\/wp-content\/uploads/\/images\/blog/g' {} +
 find . -type f -exec sed -i 's/http:\/\/www.dylanmtaylor.com\/wp-content\/uploads/images\/blog/g' {} +
-find . -type f -exec sed -i 's///g' {} +
+find . -type f -exec sed -i 's/http:\/\/dylantaylor.files.wordpress.com/images\/blog/g' {} +
+echo "use relative urls"
 find . -type f -exec sed -i 's/http:\/\/www.dylanmtaylor.com//g' {} +
+echo "correct wordpress urls"
 find . -type f -exec sed -i 's/wordpress_url:\ /wordpress_url:\ http:\/\/www.dylanmtaylor.com/g' {} +
