ce53563f6a8f70d2067151d0cb35817e2419f7b2
Dylan M Taylor <dylan.m.taylor92@gmail.com>
2015-04-20 08:32:33 +0000

    reparse

--- blog/parse_posts.sh
+++ blog/parse_posts.sh
@@ -1,3 +1,2 @@
 find . -type f -exec sed -i 's/http:\/\/dylanmtaylor.com\/wp-content\/uploads/\/images\/blog/g' {} +
-find . -type f -exec sed -i 's/http:\/\/www.dylanmtaylor.com\/wp-content\/uploads/\/images\/blog/g' {} +
-
+find . -type f -exec sed -i 's/http:\/\/www.dylanmtaylor.com\/wp-content\/uploads/images\/blog/g' {} +
