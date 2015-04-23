61ae832761fdcca6c1ed2598bba6148c6bf3c716
Dylan Taylor <dylan.m.taylor92@gmail.com>
2015-04-23 03:40:02 -0400

    for convenience, add build shell script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#First build the static content for the website
+python build_site.py
+#Copy home to index.html
+cp pages/full/home/index.html index.html
+#Finally, build the blog
+cd blog/
+jekyll build
