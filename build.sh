42b9263e18642326efc620326622a08921b7b4ec
Dylan Taylor <dylan.m.taylor92@gmail.com>
2015-04-23 04:02:23 -0400

    Use redirect instead of copying over index.html every build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #First build the static content for the website
 python build_site.py
 #Copy home to index.html
-cp pages/full/home/index.html index.html
+#cp pages/full/home/index.html index.html
 #Finally, build the blog
 cd blog/
 jekyll build
