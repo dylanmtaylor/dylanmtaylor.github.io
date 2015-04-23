3f7e419e41a094d1b5d40a463d8d1984f8b7b926
Dylan Taylor <dylan.m.taylor92@gmail.com>
2015-04-23 03:22:50 -0400

    make script actually useful

--- build.py
+++ build.py
@@ -2,27 +2,29 @@
 pages = ['home','contact', 'contact/thanks', 'donate','portfolio', 'resume'];
 titles = ['Home','Contact Me', 'Thanks for the Message', 'Donation Page','Portfolio', 'R&eacute;sum&eacute;'];
 for page, title in zip(pages,titles):
-	print "Building " + page + "..."
-	print "<!DOCTYPE HTML>\n<head>"
-	print "\t<title>Dylan M. Taylor's Personal Homepage - " + title + "</title>"
-	with open('interface/head.html','r') as f:    head = f.readlines()
+	path = "./pages/full/"+page+"/index.html"
+	f = open(path, 'w')
+	print "Building " + page + " (title: \"" + title + "\") ..."
+	f.write("<!DOCTYPE HTML>\n<head>\n")
+	f.write("\t<title>Dylan M. Taylor's Personal Homepage - " + title + "</title>\n")
+	with open('interface/head.html','r') as i:    head = i.readlines()
 	for line in head:
-		print '\t' + line,
-	print "</head>"
-	print "<body onload='prepare();>"
-	with open('interface/header.html','r') as f:    header = f.read()
-	print "\t<div class=\"header\">\n\t\t" + header + "\t</div>"
-	with open('interface/navigation.html','r') as f:    navigation = f.readlines()
-	print "\t<div class=\"left\">"
+		f.write('\t' + line)
+	f.write("</head>\n")
+	f.write("<body onload='prepare();'>\n")
+	with open('interface/header.html','r') as i:    header = i.read()
+	f.write("\t<div class=\"header\">\n\t\t" + header + "\t</div>\n")
+	with open('interface/navigation.html','r') as i:    navigation = i.readlines()
+	f.write("\t<div class=\"left\">\n")
 	for line in navigation:
-		print '\t\t' + line,
-	print "\t</div>"
-	fname = "pages/full/"+page+"/content.html"
-	with open(fname,'r') as f:    content = f.readlines()
-	print "\t<div class=\"pagecontent\" id=\"pagecontent\">"
+		f.write('\t\t' + line)
+	f.write("\t</div>\n")
+	fname = "./pages/full/"+page+"/content.html"
+	with open(fname,'r') as i:    content = i.readlines()
+	f.write("\t<div class=\"pagecontent\" id=\"pagecontent\">\n")
 	for line in content:
-		print '\t\t' + line,
-	print "\t</div>"
-	with open('interface/end.html','r') as f:    ending = f.readlines()
+		f.write('\t\t' + line)
+	f.write("\t</div>\n")
+	with open('./interface/end.html','r') as i:    ending = i.readlines()
 	for line in ending:
-		print line,
+		f.write(line)
